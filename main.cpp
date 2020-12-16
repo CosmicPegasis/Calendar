c4ac38b2fdd03a7b0ab7c857cc34167c4b4de500
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-16 20:29:23 +0530

    Add Introduction

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+void introduceGame();
 
 int main(){
-    std::cout << "Hello! This is a calendar program.";
+    introduceGame();
+}
+
+void introduceGame(){
+    std::cout << "Welcome To Calendar!" << "\nThis is the number one calendar program in the universe!";
 }
