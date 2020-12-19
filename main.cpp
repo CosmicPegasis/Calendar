295748a437382acd96a4544596e83d26059489db
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-19 19:55:12 +0530

    Wrote todo for next big part

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@ void introduceApp();
 
 int main(){
     introduceApp();
+    YearCalendar calendar;
+    calendar.printYear(2025);
 }
 
 // Introduces the app
 void introduceApp(){
-    std::cout << "Welcome To Calendar!" << "\nThis is the number one calendar program in the universe!";
+    std::cout << "Welcome To Calendar!" << "\nThis is the number one calendar program in the universe!" << std::endl;
 }
+
+// TODO [Start from here] Take user input
