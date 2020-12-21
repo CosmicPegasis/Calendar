e33937eeccd05f2925e68a9ef73d986f11ae5051
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-21 15:34:29 +0530

    Make basic menu

---
+++ user_interaction.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+namespace interaction{
+
+    // Introduces the app
+    void introduceApp(){
+        std::cout << "Welcome To Calendar!" << "\nThis is the number one calendar program in the universe!" << std::endl;
+    }
+
+    int menu(){
+        int response;
+        bool menu_running;
+        do{
+            std::cout << "Please select an option" << std::endl;
+            std::cout << "[1] Print out the calendar for a year" << std::endl;
+            std::cout << "Response: ";
+            std::cin >> response;
+
+            if(response == 1){
+                std::cout << "Yay! It works!";
+                menu_running = false;
+
+            }
+            else{
+                menu_running = true;
+            }
+        }
+        while(menu_running == true);
+
+    }
+
+}
