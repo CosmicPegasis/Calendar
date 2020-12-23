8148f12771d722dcf798065364a7ff1d50a6768f
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-23 19:38:58 +0530

    Fix JSON reading function

--- users.hpp
+++ users.hpp
@@ -1,15 +1,42 @@
-#include "json.hpp"
+#include "dependencies/json.hpp"
 #include <fstream>
+#include <iostream>
+
+#pragma once
 
 using json = nlohmann::json;
 
-struct UserProcesing{
+struct UserProcesingHelper{
     json users;
 
-    int read_json() {
-        std::ifstream file;
-        file.open("users.json");
-        file >> users;
-        file.close();
+    void read_json() {
+        std::ifstream file("users.json");
+        users = json::parse(file);
     }
+
+    bool check_user(std::string user){
+        read_json();
+        if(users.find(user) != users.end()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    bool check_password(std::string user){
+        std::cout << "Please enter your password: ";
+        std::string password;
+        std::cin >> password;
+        std::string correct_password = users.value(user, "hello");
+
+        if(correct_password == password){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
 };
