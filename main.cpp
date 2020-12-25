5776042f13235a0f3e640d56d9c1ca015ee8f112
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-25 13:51:27 +0530

    Fix file paths

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include "calendar.hpp"
+#include "features/calendar.hpp"
 #include "user_interaction.hpp"
+#include "users.hpp"
 
 
 int main(){
-    interaction::introduceApp();
-    interaction::menu();
+    //interaction::introduceApp();
+    // interaction::menu();
+    UserProcesingHelper helper;
+    helper.make_user("cosmic", "aarav");
 //    YearCalendar calendar;
 //    calendar.printYear(2025);
 }
