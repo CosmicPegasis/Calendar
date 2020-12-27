b121a5bfe85e0a1740d2f8e33e02e5d94ff173de
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-27 15:16:50 +0530

    Finish refactor

--- users.hpp
+++ users.hpp
@@ -55,44 +55,17 @@ struct UserProcessing{
     void process(){
         UserProcesingHelper helper;
         do{
-            // Ask what to do
-            std::cout << "Welcome!" << std::endl;
-                // User log in
-            std::cout << "[1] Log In" << std::endl;
-                // Make a new user
-            std::cout << "[2] Make New User" << std::endl;
-
-            std::string response;
-            std::cin >> response;
-            
+            std::string response = intro();
             if(response == "1"){
                 // Ask Credentials 
                 std::string username = get_username();
 
                 bool is_user = helper.check_user(username);
-                if(is_user) {
-                    do{
-                        // Verify Credentials
-                        // Existent user ask for password
-                        bool is_correct_password = helper.check_password(username);
+                bool authentication_sucessful;
+                authentication_sucessful = process_password(is_user, username, helper);
 
-                        if(is_correct_password){
-                            std::cout << "Hello! " << username;
-                            break;
-                        }
-
-                        else{
-                            std::cout << "Incorrect Password!";
-                            continue;
-                        }
-                    } 
-                    while(true);
-                    // TODO Continue from here -- Make case for when user doesn't exist
+                if(authentication_sucessful){
                     break;
-                }
-                else{
-                    std::cout << "This user does not exist." << std::endl;
-                    continue;
                 }
             }
         }
@@ -110,5 +83,46 @@ struct UserProcessing{
         std::cin >> username;
 
         return username;
+    }
+    std::string intro(){
+        // Ask what to do
+        std::cout << "Welcome!" << std::endl;
+            // User log in
+        std::cout << "[1] Log In" << std::endl;
+            // Make a new user
+        std::cout << "[2] Make New User" << std::endl;
+            // Quit
+        std::cout << "[3] Quit";
+
+        std::string response;
+        std::cin >> response;
+
+        return response;
+    }
+    bool process_password(bool is_user, std::string username, UserProcesingHelper helper){
+            if(is_user) {
+                do{
+                    // Verify Credentials
+                    // Existent user ask for password
+                    bool is_correct_password = helper.check_password(username);
+
+                    if(is_correct_password){
+                        std::cout << "Hello! " << username;
+                        break;
+                    }
+
+                    else{
+                        std::cout << "Incorrect Password!" << std::endl;
+                        continue;
+                    }
+                } 
+                while(true);
+                // TODO Continue from here -- Make case for when user doesn't exist
+                return true;
+            }
+            else{
+                std::cout << "This user does not exist." << std::endl;
+                return false;
+            }
     }
 };
