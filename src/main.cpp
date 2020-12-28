cb13ef1f0351ac750f31a86360796c674387d807
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-28 17:50:44 +0530

    Move to make place for test

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "features/calendar.hpp"
+#include "user_interaction.hpp"
+#include "users.hpp"
+
+// TODO Figure out what ODR violations are
+// TODO Write test cases for my app
+
+int main(){
+    interaction::introduceApp();
+    interaction::menu();
+}
