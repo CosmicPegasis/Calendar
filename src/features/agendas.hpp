22d10e79afe082bb917715bac2583596418385b3
CosmicPegasis <aviralsingh@protonmail.com>
2020-12-29 13:57:16 +0530

    Start work on agenda feature

--- src/features/agendas.hpp
+++ src/features/agendas.hpp
@@ -1,4 +1,22 @@
-// Make a user system
+#include <nlohmann/json.hpp>
+#include <fstream>
 // Store data in a file
 // Add agendas 
 // Add a help statement
+
+using json = nlohmann::json;
+
+struct AgendaHelper{
+    json agenda;
+
+    AgendaHelper(){
+        std::ifstream file_read("../data/agendas.json");
+        agenda = json::parse(file_read);
+    }
+
+    std::string make_agenda(std::string agenda_text, std::string username, int date, int month, int year){
+        agenda[username][year][month][date] = agenda_text;
+    
+        return agenda_text;
+    }
+};
--- tests/agendas.cpp
+++ tests/agendas.cpp
@@ -1,7 +1,9 @@
+#define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 #include "../src/features/agendas.hpp"
 
+// TODO Run this to figure out what the error is
 TEST_CASE("Agenda is made", "[agendas.hpp]") {
-    Agenda agenda;
-    CHECK(agenda.set_agenda("Happy Birthday", "aviral", 19, 08, 2004) == agenda.fetch_agenda("aviral", 19, 08, 2004));
+    AgendaHelper helper;
+    helper.make_agenda("Happy Birthday", "aviral", 19, 8, 2004);
 }
