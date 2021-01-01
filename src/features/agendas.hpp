1578d51b0907fe142edab1fe11e3d8dcc6bacbe3
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-01 09:48:14 +0530

    Finish make_agenda function

--- src/features/agendas.hpp
+++ src/features/agendas.hpp
@@ -1,5 +1,8 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <iostream>
+
+#pragma once
 // Store data in a file
 // Add agendas 
 // Add a help statement
@@ -10,12 +13,18 @@ struct AgendaHelper{
     json agenda;
 
     AgendaHelper(){
-        std::ifstream file_read("../data/agendas.json");
+        std::ifstream file_read;
+        file_read.open("src/data/agendas.json");
         agenda = json::parse(file_read);
+        file_read.close();
     }
 
     std::string make_agenda(std::string agenda_text, std::string username, int date, int month, int year){
-        agenda[username][year][month][date] = agenda_text;
+        agenda[username] = {year, {month, {date, agenda_text}}};
+        std::ofstream file_write;
+        file_write.open("src/features/agendas.json");
+        file_write << agenda;
+        file_write.close();
     
         return agenda_text;
     }
