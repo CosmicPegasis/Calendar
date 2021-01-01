52b1ae528906dc27f2b27457807155175eb9bec1
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-01 09:54:28 +0530

    Fix main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "user_interaction.hpp"
-#include "features/agendas.hpp"
 
 
 // TODO Figure out what ODR violations are
@@ -10,7 +9,5 @@
 int main(){
     interaction::introduceApp();
     interaction::menu();
-    //AgendaHelper helper;
-    //helper.make_agenda("Happy Birthday", "aviral", 19, 8, 2020);
 }
 
