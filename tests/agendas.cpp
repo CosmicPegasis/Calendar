084f9b6920ee189d02cc646cf87187c274db3e38
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-04 14:10:35 +0530

    Add test case for where agenda doesn't exist

--- tests/agendas.cpp
+++ tests/agendas.cpp
@@ -2,7 +2,6 @@
 #include <catch2/catch.hpp>
 #include "../src/features/agendas.hpp"
 
-// TODO Run this to figure out what the error is
 TEST_CASE("Agenda is made", "[agendas.hpp]") {
     AgendaHelper helper;
     helper.make_agenda("Happy Birthday", "aviral", "19", "8", "2004");
@@ -10,4 +9,5 @@ TEST_CASE("Agenda is made", "[agendas.hpp]") {
 TEST_CASE("Agenda is checked", "[agendas.hpp") {
     AgendaHelper helper;
     CHECK(helper.get_agenda("aviral", "19", "8", "2004") == "Happy Birthday");
+    CHECK(helper.get_agenda("cosmic", "24", "9", "2019") == "No agenda for this day.");
 }
