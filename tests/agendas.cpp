bd8e24c5257694f75291c148b3b9597ca6b92d83
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-04 14:02:14 +0530

    Add test case for make_agenda function

--- tests/agendas.cpp
+++ tests/agendas.cpp
@@ -5,5 +5,9 @@
 // TODO Run this to figure out what the error is
 TEST_CASE("Agenda is made", "[agendas.hpp]") {
     AgendaHelper helper;
-    helper.make_agenda("Happy Birthday", "aviral", 19, 8, 2004);
+    helper.make_agenda("Happy Birthday", "aviral", "19", "8", "2004");
+}
+TEST_CASE("Agenda is checked", "[agendas.hpp") {
+    AgendaHelper helper;
+    CHECK(helper.get_agenda("aviral", "19", "8", "2004") == "Happy Birthday");
 }
