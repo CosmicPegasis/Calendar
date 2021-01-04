dfdddd68ecbcd05bfef0afd825318e823bf9fbd6
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-04 14:02:54 +0530

    Add get agenda function and fix error in gettin agenda back

--- src/features/agendas.hpp
+++ src/features/agendas.hpp
@@ -30,13 +30,18 @@ struct AgendaHelper{
         file_read.close();
     }
 
-    std::string make_agenda(std::string agenda_text, std::string username, int date, int month, int year){
-        agenda[username] = {year, {month, {date, agenda_text}}};
+    std::string make_agenda(std::string agenda_text, std::string username, std::string date, std::string month, std::string year){
+        agenda[username][year][month][date] = agenda_text;
         std::ofstream file_write;
         file_write.open("src/data/agendas.json");
         file_write << agenda;
         file_write.close();
     
+        return agenda_text;
+    }
+
+    std::string get_agenda( std::string username, std::string date, std::string month, std::string year) {
+        std::string agenda_text = agenda[username][year][month][date];
         return agenda_text;
     }
 };
