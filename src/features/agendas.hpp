89248eed145cc50bf0d44e86eaf2c9af171b7b41
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-04 14:18:19 +0530

    Add comments

--- src/features/agendas.hpp
+++ src/features/agendas.hpp
@@ -9,38 +9,52 @@
 
 using json = nlohmann::json;
 
+// Base class
 struct AgendaHelper{
+    // Make json var using json header file
     json agenda;
 
     AgendaHelper(){
+        // Open a file stream to read file
         std::ifstream file_read;
         file_read.open("src/data/agendas.json");
+        // Checks if file doesn't exist
         if(!file_read){
+            // Closes file to avoid corruption
             file_read.close();
 
+            // Opens new stream to write empty json var to new file
             std::ofstream file_make;
             file_make.open("src/data/agendas.json");
             file_make << "{}";
             file_make.close();
 
+            // Reopens the file
             file_read.open("src/data/agendas.json");
             
         }
+        // Parse the file
         agenda = json::parse(file_read);
         file_read.close();
     }
 
+    // Internal function to make agendas
     std::string make_agenda(std::string agenda_text, std::string username, std::string date, std::string month, std::string year){
+        // Assigns agenda
         agenda[username][year][month][date] = agenda_text;
+        // Writes entire json var to file
         std::ofstream file_write;
         file_write.open("src/data/agendas.json");
         file_write << agenda;
         file_write.close();
     
+        // Returns the agenda_text for uniformity and testing
         return agenda_text;
     }
 
+    // Internal function to get agenda for a fixed date
     std::string get_agenda( std::string username, std::string date, std::string month, std::string year) {
+        // Enclosed in try block because json header throws and exits
         try{
         std::string agenda_text = agenda[username][year][month][date];
         return agenda_text;
@@ -50,3 +64,5 @@ struct AgendaHelper{
         }
     }
 };
+
+// TODO [Continue from here] Write AgendaHelper wrapper
