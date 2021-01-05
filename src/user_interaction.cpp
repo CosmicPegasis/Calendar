b367736ec5b67bafe5b1abe3b41a5262c02d2107
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-05 19:33:12 +0530

    Restructure to comply with better pratice

---
+++ src/user_interaction.cpp
@@ -0,0 +1,30 @@
+#include "user_interaction.hpp"
+
+void interaction::introduceApp(){
+        std::cout << "Welcome To Calendar!" << "\nThis is the number one calendar program in the universe!" << std::endl;
+    }
+
+void interaction::menu(){
+    std::string response;
+    do{
+        std::cout << "Please select an option" << std::endl;
+        std::cout << "[1] Print out the calendar for a year" << std::endl;
+        std::cout << "[2] Check out your TODOs and agendas" << std::endl;
+        std::cout << "Response: ";
+        std::cin >> response;
+
+        if(response == "1"){
+            YearCalendar calendar;
+            calendar.menu();
+            break;
+
+        }
+        if(response == "2") {
+            UserProcessing processor;
+            processor.process();
+            break;
+        }
+    }
+    while(true);
+
+}
--- src/user_interaction.hpp
+++ src/user_interaction.hpp
@@ -7,33 +7,7 @@
 namespace interaction{
 
     // Introduces the app
-    void introduceApp(){
-        std::cout << "Welcome To Calendar!" << "\nThis is the number one calendar program in the universe!" << std::endl;
-    }
+    void introduceApp();
 
-    void menu(){
-        std::string response;
-        do{
-            std::cout << "Please select an option" << std::endl;
-            std::cout << "[1] Print out the calendar for a year" << std::endl;
-            std::cout << "[2] Check out your TODOs and agendas" << std::endl;
-            std::cout << "Response: ";
-            std::cin >> response;
-
-            if(response == "1"){
-                YearCalendar calendar;
-                calendar.menu();
-                break;
-
-            }
-            if(response == "2") {
-                UserProcessing processor;
-                processor.process();
-                break;
-            }
-        }
-        while(true);
-
-    }
-
-}
+    void menu();
+};
