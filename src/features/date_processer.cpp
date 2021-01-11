b6ddc9d79633239fe4e32a0cfef33e95e056f2a7
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-11 14:09:49 +0530

    Fix pseudocode

--- src/features/date_processer.cpp
+++ src/features/date_processer.cpp
@@ -8,22 +8,18 @@ bool DateProcesser::check_date_validity(std::string date){
 
 bool DateProcesser::check_year_validity(std::string year){
     // Check year validity 
-        // Slice the year part from the string
         // Also check whether the year is a leap year
 
 }
 
 bool DateProcesser::check_month_validity(std::string month){
     // Check month validity
-        // Slice month part from the code 
         // Also check what the last date is
 }
 
 bool DateProcesser::check_month_date_validity(std::string month_date){
 
     // Check date validity
-        // Index the first two digits
-            // Convert to int
             // Check the date is correct or not in comparison to the month
     
 }
