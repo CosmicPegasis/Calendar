81176c1c4912a5f01780bf9944a2582a552ea22c
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-21 14:55:01 +0530

    Correct formatting

--- include/date_processer.hpp
+++ include/date_processer.hpp
@@ -3,20 +3,20 @@
 
 #pragma once
 
-struct DateProcesser{
-    DateProcesser(std::string date);
+struct DateProcesser {
+  DateProcesser(std::string date);
 
-    void check_date_validity();
+  void check_date_validity();
 
-    bool check_month_date_validity();
+  bool check_month_date_validity();
 
-    bool check_month_validity();
+  bool check_month_validity();
 
-    bool check_year_validity();
+  bool check_year_validity();
 
-    bool is_valid{true};
-    const std::string date;
-    const std::string month_date;
-    const std::string month;
-    const std::string year;
+  bool is_valid{true};
+  const std::string date;
+  const std::string month_date;
+  const std::string month;
+  const std::string year;
 };
--- src/features/date_processer.cpp
+++ src/features/date_processer.cpp
@@ -1,47 +1,40 @@
 #include "date_processer.hpp"
 
-// TODO Add a date constructor to take in date and then change all parameters to just the date
-// Idea on how to easily verify dates - use the prebuilt date caluclation from agendas and then match the return date to user given date.
+// TODO Add a date constructor to take in date and then change all parameters to
+// just the date Idea on how to easily verify dates - use the prebuilt date
+// caluclation from agendas and then match the return date to user given date.
 
 DateProcesser::DateProcesser(std::string date)
-	: date{date},
-	month_date {date.substr(0, 2)},
-	month {date.substr(3, 2)},
-	year {date.substr(6, 4)}
-	{}
+    : date{date}, month_date{date.substr(0, 2)}, month{date.substr(3, 2)},
+      year{date.substr(6, 4)} {}
 
-
-void DateProcesser::check_date_validity(){
-    if(date.length() != 10){
-        is_valid = false;
-    }
-    else if(date.substr(2, 1) != "/" || date.substr(5, 1) != "/"){
-        is_valid = false;
-    }
+void DateProcesser::check_date_validity() {
+  if (date.length() != 10) {
+    is_valid = false;
+  } else if (date.substr(2, 1) != "/" || date.substr(5, 1) != "/") {
+    is_valid = false;
+  }
 }
 
-bool DateProcesser::check_year_validity(){
-    std::stringstream ss{year};
-    int year;
-    try{
-        ss >> year;
-    }
-    catch(std::exception &e){
-        is_valid = false;
-    }
-    // Check year validity 
-        // Also check whether the year is a leap year
-
+bool DateProcesser::check_year_validity() {
+  std::stringstream ss{year};
+  int year;
+  try {
+    ss >> year;
+  } catch (std::exception &e) {
+    is_valid = false;
+  }
+  // Check year validity
+  // Also check whether the year is a leap year
 }
 
-bool DateProcesser::check_month_validity(){
-    // Check month validity
-        // Also check what the last date is
+bool DateProcesser::check_month_validity() {
+  // Check month validity
+  // Also check what the last date is
 }
 
-bool DateProcesser::check_month_date_validity(){
+bool DateProcesser::check_month_date_validity() {
 
-    // Check date validity
-            // Check the date is correct or not in comparison to the month
-    
+  // Check date validity
+  // Check the date is correct or not in comparison to the month
 }
