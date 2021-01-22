37723ca4871ef49cc5f2c2dbba06a73d82541e36
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-22 19:16:30 +0530

    Finish chech_year_validity()

--- src/features/date_processer.cpp
+++ src/features/date_processer.cpp
@@ -1,14 +1,18 @@
 #include "date_processer.hpp"
 
 // TODO Add a date constructor to take in date and then change all parameters to
-// just the date Idea on how to easily verify dates - use the prebuilt date
+// just the date
+//
+// TODO Add error messages and handle errors gracefully
+
+// Idea on how to easily verify dates - use the prebuilt date
 // caluclation from agendas and then match the return date to user given date.
 
 DateProcesser::DateProcesser(std::string date)
     : date{date}, month_date{date.substr(0, 2)}, month{date.substr(3, 2)},
       year{date.substr(6, 4)} {}
 
-void DateProcesser::check_date_validity() {
+std::string DateProcesser::check_date_validity() {
   if (date.length() != 10) {
     is_valid = false;
   } else if (date.substr(2, 1) != "/" || date.substr(5, 1) != "/") {
@@ -16,7 +20,7 @@ void DateProcesser::check_date_validity() {
   }
 }
 
-bool DateProcesser::check_year_validity() {
+void DateProcesser::check_year_validity() {
   std::stringstream ss{year};
   int year;
   try {
@@ -25,7 +29,13 @@ bool DateProcesser::check_year_validity() {
     is_valid = false;
   }
   // Check year validity
+  if (year < 1000 || year > 9999) {
+    is_valid = false;
+  }
   // Also check whether the year is a leap year
+  else if (year % 4 == 0) {
+      
+  }
 }
 
 bool DateProcesser::check_month_validity() {
