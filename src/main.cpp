4ac7c566802684b7bb2509809201c2d5547ef07a
CosmicPegasis <aviralsingh@protonmail.com>
2021-01-25 16:21:43 +0530

    Finish all TODOs

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,6 @@
 #include "user_interaction.hpp"
 
 
-// TODO Make wrapper for AgendaHelper
-// TODO Test add agenda part of AgendaHelper wrapper
-// TODO Set required functions to const
-
 int main() {
     interaction::introduceApp();
     interaction::menu();
